16c3264774f398625ed2c50454dbe6395eb27284
Nik Van Baak <nvanbaak@gmail.com>
2022-10-06 18:35:47 -0500

    slight efficiency adjustment to ec a

--- capaul-stuff/extra_credit_problems/a.py
+++ capaul-stuff/extra_credit_problems/a.py
@@ -4,27 +4,26 @@
 # STRATEGY
 #   Each level of the tetrahedron is a triangle
 #   Number of cannonballs in a triangle of size n = 1 + ... + n
-#   But we don't wanna do that on every level, so we'll use DP to store old results
+#   But we don't wanna do that on every level, so we'll store previous results
 
 import sys
 
 num_lines = int(sys.stdin.readline().strip())
 for input in range(num_lines):
 
-    levels = []
     n = int(sys.stdin.readline().strip())
     total = 0
+    prev_level = 0
 
     for level in range(n):
         if level == 0:
-            levels.append(1)
+            prev_level = 1
             total = 1
         else:
             # the area of a triangle of side length n is equal to
             # n plus the area of a triangle with side length n-1
-            prev_level = levels[level-1]
             curr_level = prev_level + level + 1
-            levels.append(curr_level)
             total += curr_level
+            prev_level = curr_level
 
     print(f"{input+1}: {n} {total}")
--- capaul-stuff/extra_credit_problems/check_answers.sh
+++ capaul-stuff/extra_credit_problems/check_answers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 $1.py < IO/$1.in > $1-output.txt
-diff $1-output.txt IO/$1.out --report-identical-files
-rm $1-output.txt
+python3 $1.py < IO/$1.in > $1-test-output.txt
+diff $1-test-output.txt IO/$1.out --report-identical-files
+rm $1-test-output.txt
