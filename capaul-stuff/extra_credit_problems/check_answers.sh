a73b6831fd17464b7327a70285feaff6c7a5885e
Nik Van Baak <nvanbaak@gmail.com>
2022-10-06 19:26:30 -0500

    added timer to bash script

--- capaul-stuff/extra_credit_problems/check_answers.sh
+++ capaul-stuff/extra_credit_problems/check_answers.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-python3 $1.py < IO/$1.in > $1-test-output.txt
-diff $1-test-output.txt IO/$1.out --report-identical-files -y --suppress-common-lines
-rm $1-test-output.txt
+start_time=`python3 -c 'from time import time; print(int(round(time() * 1000)))'`
+python3 $1.py < IO/$1.in > $1-test.out
+end_time=`python3 -c 'from time import time; print(int(round(time() * 1000)))'`
+time_elapsed=`echo "$end_time - $start_time" | bc`
+echo "Time: $time_elapsed ms"
+
+diff $1-test.out IO/$1.out --report-identical-files -y --suppress-common-lines
+rm $1-test.out
